0bdea2616e76a82f1f7b8479abdd0dee4123a1d6
Alexandr <0719118@gmail.com>
2021-04-02 14:57:52 +0300

    lesson-52(Ajax - GET request)

---
+++ js-52/index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ajax GET request</title>
+    <style>
+        input{
+            width: 400px;
+            height: 50px;
+            font-size: 32px;
+        }
+    </style>
+</head>
+<body>
+<form action="">
+
+    <h2>UAH</h2>
+    <div>
+        <input type="number" name="uah" id="uah" />
+    </div>
+
+    <h2>USD</h2>
+    <div>
+        <input type="text" name="usd" id="usd" />
+    </div>
+
+</form>
+<script src="js/script.js"></script>
+</body>
+</html>
+
---
+++ js-52/js/script.js
@@ -0,0 +1,47 @@
+'use strict';
+const inputUah = document.querySelector('#uah'),
+      inputUsd = document.querySelector('#usd');
+
+inputUah.addEventListener('input', (event) => {
+   event.preventDefault();
+
+   const request = new XMLHttpRequest(); // экземпляр класса для запроса
+
+    /*open(method, url)
+            method - определяет метод запроса (GET или POST)
+            url - путь к серверу (файл или реальный сервер. без разницы)
+            Ajax запросы явзялются по умолчанию асинхронным кодом
+            (который не блокирует работу остального кода)
+    */
+
+    // метод для настроек, которые в будущем помогут сделать запрос
+    request.open('GET', 'js/current.json');
+
+   /* уточняем запрос. что именно отправляем и какая кодировка
+    чтоб сервер понимал, что он принимает в себя (json файл или изображение)
+    setRequestHeader - заголовок для передачи json файлов
+    кодировку устанавливать не обязательно*/
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+    //отправляем запрос
+    request.send();
+
+    //событие load - срабатывает только один раз, когда запрос полностью готов
+    request.addEventListener('load', () => {
+        if(request.status == 200){
+            //request.response - тут лежит ответ от сервера в json формате
+            //JSON.parse - перевожу json объект в обычный js объект
+            const data = JSON.parse(request.response);
+            inputUsd.value = (+inputUah.value / data.current.usd).toFixed(2);
+        }else{
+            inputUsd.value = 'Error connecting with server'
+        }
+    });
+
+    //status- получение статуса от сервера
+
+    //response - ответ от сервера (код, который буду использовать)
+
+    //readyState - текущее состояне запроса (0,1,2,3,4)
+
+});
