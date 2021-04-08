e60832a406669715020d6a476fa5e49367f22446
Alexandr <0719118@gmail.com>
2021-04-08 15:23:28 +0300

    lesson-62 (slider-2)

--- js-58-plus/Food/src/js/main.js
+++ js-58-plus/Food/src/js/main.js
@@ -280,25 +280,30 @@ document.addEventListener('DOMContentLoaded', () => {
     const current = document.querySelector('#current');
     const total = document.querySelector('#total');
     const slides = document.querySelectorAll('.offer__slide');
+    const slidesWrapper = document.querySelector('.offer__slider-wrapper');
+    const inner = document.querySelector('.offer__slider-inner');
+    const width = window.getComputedStyle(slidesWrapper).width;
     let counter = 0;
+    let offset = 0;
 
+    //увеличил ширину слайдера на колличество слайдов
+    inner.style.width = parseInt(width) * slides.length + '%';
+
+    //каждому слайду присвоил одинаковую ширину (как у родителя)
+    slides.forEach(slide => {
+        slide.style.width = width;
+    });
+
+    //вывел колличество слайдов
     total.innerHTML = (slides.length < 10) ? `0${+slides.length}` : +slides.length;
 
-    function start() {
-        slides.forEach((slide, i, arr) => {
-            slide.classList.add('hide');
-            arr[0].classList.remove('hide');
-        });
-
-        current.innerHTML = (counter < 10) ? `0${counter+1}` : counter+1;
-    }
-    start();
+    //вывел текущий слайд
+    current.innerHTML = (counter < 10) ? `0${counter+1}` : counter+1;
 
     function showSlide(index){
-        slides.forEach((slide, i, arr) => {
-            slide.classList.add('hide');
-            arr[index].classList.remove('hide', 'current');
-        });
+        offset = parseInt(width) * index;
+
+        inner.style.transform = `translateX(-${offset}px)`;
 
         current.innerHTML = (counter < 10) ? `0${counter+1}` : counter+1;
     }
